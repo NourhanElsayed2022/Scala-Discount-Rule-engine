ddad889f4e06627bd253083438103b80b306d5ce
NourhanElsayed2022 <nourradwan34@gmail.com>
2024-05-07 18:37:12 +0300

    Adding new requirements and modify the output format

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# Scala-Discount-Rule-engine
+# Scala Discounts Engine
+
+This Scala project calculates discounts for orders based on various criteria such as expiration date, product category, purchase channel, payment method, and quantity purchased.
+
+## **Project Details:**
+
+This project implements a discount calculation engine in Scala. It reads order data from a CSV file, applies various discount rules, and writes the discounted orders to an output CSV file. The discount rules include expiration date discounts, category-based discounts, special discounts for orders placed on specific dates, quantity-based discounts, app purchase discounts, and payment method discounts.
+
+## **Discount Calculation:**
+
+Discounts are calculated based on the following criteria:
+
+- **Expiration Date Discount:** If the number of days remaining between the purchasing date and the expiration date is less than 30 days, a discount of (30 - (days between)) is applied. Otherwise, no discount is applied.
+
+For example:
+
+- If 29 days remain ‐> 1% discount.
+- If 28 days remain ‐> 2% discount.
+- If 27 days remain ‐> 3% discount. etc …
+- **Product Category Discount:** If the product category is "Cheese", a discount of 10% is applied. And If the product category is "Wine", a discount of 5% is applied. Otherwise, no discount is applied.
+- **Specific Day of Month Discount:** If the purchasing date is March 23, a special discount of 50% is applied. Otherwise, no discount is applied.
+- **Quantity Discount:** If the quantity of products purchased is more than 5 of the same product, a discount will be applied.
+    - If the quantity is between 6 and 9, a discount of 5% is applied.
+    - If the quantity of products purchased is between 10 and 14, a discount of 7% is applied.
+    - If the quantity of products purchased is 15 or more, a discount of 10% is applied.
+    - Otherwise, no discount is applied.
+- **Purchasing Channel Discount:** Sales that are made through the "App" will have a special discount. A discount of the quantity rounded up to the nearest multiple of 5 is applied. Otherwise, no discount is applied.
+
+For example:
+
+- If quantity: 1, 2, 3, 4, 5 ‐> discount 5%.
+- If quantity 6, 7, 8, 9, 10 ‐> discount 10%.
+- If quantity 11, 12, 13, 14, 15 ‐> discount 15%. etc …
+- **Payment Method Discount:** If the payment method is "Visa", a discount of 5% is applied. Otherwise, no discount is applied
+
+## **Main Rules:**
+
+- Transactions that didn't qualify for any discount will have a 0% discount.
+- Transactions that qualified for more than one discount will get the top 2 and get their average.
+- After ingesting the data and calculating the discount the final price will be calculated and loaded into the output files.
+
+## **Project Main Functions:**
+
+- **calcExpireDiscount:** Calculates the discount to be applied to a product based on its expiration date.
+- **categoryDiscount:** Determines the discount to be applied to a product based on its category.
+- **the23rd_of_March:** Determines the discount to be applied to a purchase made on 23 march.
+- **quantitybasedDiscoun**t : Determines the discount to be applied to a product based on its purchased quantity.
+- **appDiscount:** Determines the discount to be applied to a purchase based on the purchasing channel.
+- **paymentDiscount:** Determines the discount to be applied to a purchase based on the payment method.
+- **calculateDiscount:** Determines the final price after applying the discount.
+
+## **How does It work?**
+
+This Scala project calculates discounts for orders based on various criteria such as expiration date proximity, product category, special dates, quantity, app purchases, and payment method. It reads order data from a CSV file (**`TRX1000.csv`**) , applies the discount rules to each order, and writes the discounted orders to another CSV file. (**`DiscountOutput.csv`**).
+
+ The process is logged, indicating the initialization and completion of the discount calculation.
+
+In the format :       `TIMESTAMP LOGLEVEL MESSAGE`
+
+## **Project Main Files:**
+
+- **`TRX1000.csv`** Contains sample data to be used on the Rule Engine.
+- **`DiscountOutput.csv`** Contains the output result with discount percentage and the total price after discount.
+- `rules_engine.log` contains the logs of the Rule Engine project.
+
+## **Tools and Technologies:**
+
+- Scala Programming Language.
+- IntelliJ IDE.
+
+## **Dependencies:**
+
+- Scala 2.13.13
+- Dependencies
+
+```scala
+libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % "2.14.1"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.14.1"
+```
+
+## **How To Use**
+
+1. Install dependencies ( Scala, `log4j-1.2-api-2.23.1.jar` , `log4j-core-2.23.1.jar` ).
+2. Configure the `log4j2.xml` file in the `src/main/Resources` directory if needed.
+3. Prepare your data file (`TRX1000.csv`) with the required columns.
+4. Run the project using IDE like (IntelliJ) or Command Line.
+5. View the logs in the `logs` directory for processing details.
+
+## The Raw Data:
+
+![in.JPG](Scala%20Discounts%20Engine%2074f17010014647368ca78bd6c1618717/in.jpg)
+
+## The Output:
+
+![out.JPG](Scala%20Discounts%20Engine%2074f17010014647368ca78bd6c1618717/out.jpg)
--- src/main/scala/CalcDiscount.scala
+++ src/main/scala/CalcDiscount.scala
@@ -1,5 +1,5 @@
+// Importing required libraries
 import com.sun.javafx.geom.transform.Identity
-
 import scala.io.{BufferedSource, Source}
 import java.io.{File, FileOutputStream, PrintWriter}
 import java.time.LocalDate
@@ -8,9 +8,7 @@ import java.time.temporal.ChronoUnit
 import org.apache.logging.log4j.LogManager
 
 
-
-object CalcDiscount  extends  App{
-
+object CalcDiscount extends App {
 
   // Initialize logger
   private val logger = LogManager.getLogger(getClass.getName)
@@ -24,62 +22,52 @@ object CalcDiscount  extends  App{
   val source: BufferedSource = Source.fromFile("src/main/resources/TRX1000.csv")
   val lines: List[String] = source.getLines().drop(1).toList // drop header
 
-
+  // Output file setup
   val f: File = new File("src/main/resources/DiscountOutput.csv")
   val writer = new PrintWriter(new FileOutputStream(f, true))
 
+  // Define a case class to represent the orders
+  case class Order(orderDate: LocalDate, productName1: String,productName2: Array[String], expiryDate: LocalDate, quantity: Int, unit_price: Double, channel: String, payment_method: String, discount: Double)
 
-  // Define a case class to represent an order
-  case class Order(orderDate: LocalDate,productName: String, expiryDate: LocalDate,  quantity: Int ,unit_price: Double ,channel: String,payment_method: String ,discount :Double)
-
-  // The needed Variables
+  // Define the Date formatter
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-  // creating object from the Order class
+  // Function to convert each line to an Order object
   def toOrder(line: String): Order = {
     val orderDateStr = line.split(",")(0).split("T")(0)
     val expiryDateStr = line.split(",")(2)
-
     val orderDate = LocalDate.parse(orderDateStr, dateFormatter)
     val expiryDate = LocalDate.parse(expiryDateStr, dateFormatter)
-
-    val productName = line.split(",")(1).split("-")(0)
+    val productName1 = line.split(",")(1).split("-")(0)
+    val productName2 = line.split(",")(1).split("-").tail
     val quantity = line.split(",")(3).toInt
-
     val price = line.split(",")(4).toDouble
-
     val channel = line.split(",")(5)
     val payment_method = line.split(",")(6)
-
-    Order(orderDate,  productName, expiryDate, quantity ,price ,channel ,payment_method,0)
+    Order(orderDate, productName1,productName2, expiryDate, quantity, price, channel, payment_method, 0)
   }
 
-  // create list of order objects
-  val ordersList = {
-    lines.map(toOrder(_))
-  }
+  // Create list of order objects
+  val ordersList = lines.map(toOrder(_))
 
-
-
-  // Calculate the days between the order date and the expiry date
+  // Function to Calculate the days between the order date and the expiry date
   def getDaysBetween(orderDate: LocalDate, expiryDate: LocalDate): Long = {
     ChronoUnit.DAYS.between(orderDate, expiryDate)
   }
 
-  // Calculate the discount based on the expiration days
+  // function to Calculate the discount based on the expiration days
   def calcExpireDiscount(order: Order): Double = {
     val daysBetween = ChronoUnit.DAYS.between(order.orderDate, order.expiryDate)
     if (daysBetween < 30 && daysBetween > 0) {
-      (30 - daysBetween )
+      (30 - daysBetween)
     } else {
       0
     }
   }
 
-  // Calculate the discount based on specific categories
+  // Function to Calculate the discount based on specific categories
   def categoryDiscount(productName: String): Double = {
     val category = productName.split("-")(0).trim.toLowerCase
-
     category match {
       case "cheese" => 10
       case "wine" => 5
@@ -87,7 +75,7 @@ object CalcDiscount  extends  App{
     }
   }
 
-  // Calculate the discount for orders placed on the 23rd of March
+  // Function to Calculate the discount for orders placed on the 23rd of March
   def the23rd_of_March(orderDate: LocalDate): Double = {
     if (orderDate.getDayOfMonth == 23 && orderDate.getMonthValue == 3) {
       50
@@ -96,30 +84,65 @@ object CalcDiscount  extends  App{
     }
   }
 
-  // Calculate the discount based on the quantity sold from the product
+  // Function to Calculate the discount based on the quantity sold from the product
   def quantitybasedDiscount(quantity: Int): Double = {
-    if(quantity > 6 && quantity < 9) {
+    if (quantity > 6 && quantity <= 9) {
       5
-    }
-    else if(quantity > 10 && quantity < 14) {
+    } else if (quantity > 10 && quantity <= 14) {
       7
-    }
-    else if(quantity > 15) {
+    } else if (quantity >= 15) {
       10
-    }
-    else {
+    } else {
       0
     }
   }
 
-  // Apply qualifiers and calculate discount
-  def calculateDiscount(order: Order): Double = {
-    val discounts = List(calcExpireDiscount(order),
-      categoryDiscount(order.productName),
-      the23rd_of_March(order.orderDate),
-      quantitybasedDiscount(order.quantity)
-    )
+  // Function to Calculate The discount for the Products that sold through the Application
+  def appDiscount(order: Order): Double = {
+    if (order.channel.toLowerCase == "app") {
+      // Round up the quantity to the nearest multiple of 5
+      val roundedQuantity = Math.ceil(order.quantity.toDouble / 5) * 5
+      if (roundedQuantity == 5) {
+        5
+      } else if (roundedQuantity == 10) {
+        10
+      } else if (roundedQuantity == 15) {
+        15
+      } else {
+        roundedQuantity
+      }
+    } else {
+      0
+    }
+  }
 
+  // Function to Calculate The discount for the Products that Purchased using Visa
+  def paymentDiscount(paymentMethod: String): Double = {
+    if (paymentMethod.toLowerCase == "visa") {
+      5
+    } else {
+      0
+    }
+  }
+
+  /*
+  This Function Calculate the final discount for each order based on 3 steps:
+          1- Apply all the discount functions and set their results in a list
+          2- Filter te list to keep the their results greater than 1 in the new list "qualifiedDiscounts"
+           Note: if the function result greater 1 , this mean that the order is Qualified fot this Discount
+          3- If the qualifiedDiscounts list size =0 , this mean that the order will not have any discount
+           --> If the size =1 , there is only one  discount and will be applied on the order.
+           --> If the size more than 1 , the greats two average will be applied on the order.
+   */
+  def calculateDiscount(order: Order): Double = {
+    val discounts = List(
+      calcExpireDiscount(order),
+      categoryDiscount(order.productName1),
+      the23rd_of_March(order.orderDate),
+      quantitybasedDiscount(order.quantity),
+      appDiscount(order),
+      paymentDiscount(order.payment_method)
+    )
     val qualifiedDiscounts = discounts.filter(_ > 0)
     if (qualifiedDiscounts.isEmpty) {
       0
@@ -133,29 +156,26 @@ object CalcDiscount  extends  App{
   // Calculate discounts for all orders
   val discountedOrders: List[Order] = ordersList.map { order =>
     val discount = calculateDiscount(order)
-    val newPrice = order.unit_price-discount
-    order.copy(unit_price = newPrice,discount = discount) // copy the object to have all values except the one I Changed(Discount)
+    order.copy(discount = discount) // Copy the object to have all values except the one I Changed(Discount)
   }
+
+  // Write headers to the output file
+  val header = "Order Date,Product Name,Expiry Date,Quantity,Unit Price,Channel,Payment Method,Order Before Discount,Discount,Order After Discount"
+  writer.println(header)
 
   // Write discounted orders to the output file
   discountedOrders.foreach { order =>
-    val line = s"${order.orderDate},${order.productName},${order.expiryDate},${order.quantity},${order.unit_price},${order.channel},${order.payment_method},${order.discount}"
+    // Format the line for writing to the output file
+    val orderBeforeDiscount = BigDecimal(order.quantity * order.unit_price).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val orderAfterDiscount = BigDecimal(orderBeforeDiscount - (orderBeforeDiscount * order.discount / 100)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val line = s"${order.orderDate},${order.productName1}-${order.productName2.mkString(" ")},${order.expiryDate},${order.quantity},${order.unit_price},${order.channel},${order.payment_method},${orderBeforeDiscount},${order.discount}%,${orderAfterDiscount}"
     writer.println(line)
   }
 
+  // Close the output file writer
   writer.close()
 
-  ///  ***********************
+//********************************************************************************************//
   // Log completion
   logger.info("Discount calculation completed successfully")
-
-
 }
-
-
-
-
-
-
-
-
